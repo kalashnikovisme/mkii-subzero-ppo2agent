b358ddd9ca9524dc73e4ec2c37a377829eabceca
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-06-20 14:11:24 +0400

    Training attempt 2

--- train.py
+++ train.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
     env = SubprocVecEnv([make_env] * n_cpu)
     env = VecFrameStack(env, n_stack=4)
 
-    model = PPO2(CnnLstmPolicy, env, n_steps=128, verbose=1, tensorboard_log="./tboard_log")   
+    # model = PPO2(CnnLstmPolicy, env, n_steps=128, verbose=1, tensorboard_log="./tboard_log")   
     # Use this if you want to continue training a saved model
-    #model = PPO2.load("training_checkpoints/mk3-ppo2_198400_steps.zip", tensorboard_log="./tboard_log")
-    #model.set_env(env)
+    model = PPO2.load("training_checkpoints/mk3-ppo2_17600_steps.zip", tensorboard_log="./tboard_log")
+    model.set_env(env)
 
     checkpoint_callback = CheckpointCallback(save_freq=4000, save_path='./training_checkpoints',
                                             name_prefix='mk3-ppo2')
