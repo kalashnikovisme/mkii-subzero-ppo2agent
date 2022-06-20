4f73549d66191014d904430aa389bf98455aa7b8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-06-20 14:31:46 +0400

    Training attempt 3

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     model = PPO2.load("training_checkpoints/mk3-ppo2_17600_steps.zip", tensorboard_log="./tboard_log")
     model.set_env(env)
 
-    checkpoint_callback = CheckpointCallback(save_freq=4000, save_path='./training_checkpoints',
+    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./training_checkpoints',
                                             name_prefix='mk3-ppo2')
-    model.learn(total_timesteps=20000, callback=checkpoint_callback)
+    model.learn(total_timesteps=62400, callback=checkpoint_callback)
     model.save('mk3-ppo2')
     env.close()
