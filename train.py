a24151dddfbe828079bea2a2596316ba9e6ce360
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-06-22 01:19:57 +0400

    Fix

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     # model = PPO2(CnnLstmPolicy, env, n_steps=128, verbose=1, tensorboard_log="./tboard_log")   
     # Use this if you want to continue training a saved model
-    model = PPO2.load("training_checkpoints/40k.zip", tensorboard_log="./tboard_log")
+    model = PPO2.load("training_checkpoints/20k.zip", tensorboard_log="./tboard_log")
     model.set_env(env)
 
     total_timesteps = 20000
