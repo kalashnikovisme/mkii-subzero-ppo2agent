4904cbc4f7126d81275ef17ccebbe07ee3b0b57f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-06-23 13:10:23 +0400

    v0.6.1 fix

--- utils.py
+++ utils.py
@@ -39,28 +39,27 @@ class ShangTsungDiscretizer(Discretizer):
         LEFT = 'LEFT'
         RIGHT = 'RIGHT'
 
-       super().__init__(env = env, buttons = env.unwrapped.buttons,
-               combos = [
-                   [],
-                   [LEFT, LEFT, LEFT, LEFT ]
-                   # [HP], [LP], [HK], [LK], [BL], [UP], [DOWN], [LEFT], [RIGHT], # single moves
-                   # [UP, HK], [LEFT, UP, HK], [RIGHT, UP, HK], # jumps
-                   # [UP, HP], [LEFT, UP, HP], [RIGHT, UP, HP], # jumps
-                   # [LEFT, HP], [RIGHT, HP], [LEFT, DOWN, HP], [RIGHT, DOWN, HP],
-                   # # [LEFT, LP], [RIGHT, LP], [LEFT, UP, LP], [RIGHT, UP, LP], [LEFT, DOWN, LP], [RIGHT, DOWN, LP],
-                   # [DOWN, HP],
-                   # [DOWN, LP], 
-                   # [DOWN, LK],
-                   # [DOWN, BL],
-                   # [LEFT, LEFT, HP], [RIGHT, RIGHT, HP], # Fire Skull
-                   # [LEFT, LEFT, RIGHT, HP], [RIGHT, RIGHT, LEFT, HP], # Double Fire Skull
-                   # [LEFT, LEFT, RIGHT, RIGHT, HP], [RIGHT, RIGHT, LEFT, LEFT, HP] # Triple Fire Skull
-                   # [RIGHT, LEFT, LEFT, LK], [LEFT, RIGHT, RIGHT, LK], # Volcanic Eruption
-                   # [HP, HP, LP], [LK, HP, HP, LP], # kombos
-                   # [HP, HP, HP, HP], [LP, LP, LP, LP], # unfair combo
-                   # [LEFT, LK], [RIGHT, LK], # podnozhka :)
-                   # [DOWN, HP] # uppercut
-                ]
+       super().__init__(env = env, buttons = env.unwrapped.buttons, combos = [
+           [],
+           [LEFT, LEFT, LEFT, LEFT ]
+           # [HP], [LP], [HK], [LK], [BL], [UP], [DOWN], [LEFT], [RIGHT], # single moves
+           # [UP, HK], [LEFT, UP, HK], [RIGHT, UP, HK], # jumps
+           # [UP, HP], [LEFT, UP, HP], [RIGHT, UP, HP], # jumps
+           # [LEFT, HP], [RIGHT, HP], [LEFT, DOWN, HP], [RIGHT, DOWN, HP],
+           # # [LEFT, LP], [RIGHT, LP], [LEFT, UP, LP], [RIGHT, UP, LP], [LEFT, DOWN, LP], [RIGHT, DOWN, LP],
+           # [DOWN, HP],
+           # [DOWN, LP], 
+           # [DOWN, LK],
+           # [DOWN, BL],
+           # [LEFT, LEFT, HP], [RIGHT, RIGHT, HP], # Fire Skull
+           # [LEFT, LEFT, RIGHT, HP], [RIGHT, RIGHT, LEFT, HP], # Double Fire Skull
+           # [LEFT, LEFT, RIGHT, RIGHT, HP], [RIGHT, RIGHT, LEFT, LEFT, HP] # Triple Fire Skull
+           # [RIGHT, LEFT, LEFT, LK], [LEFT, RIGHT, RIGHT, LK], # Volcanic Eruption
+           # [HP, HP, LP], [LK, HP, HP, LP], # kombos
+           # [HP, HP, HP, HP], [LP, LP, LP, LP], # unfair combo
+           # [LEFT, LK], [RIGHT, LK], # podnozhka :)
+           # [DOWN, HP] # uppercut
+         ]
        )
 
 gamename = "MortalKombat3-Genesis"
