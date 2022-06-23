49f11a51e53f78e02fa321540500dc72727460ec
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-06-23 13:54:41 +0400

    v0.6.1 RUN

--- utils.py
+++ utils.py
@@ -60,7 +60,11 @@ class ShangTsungDiscretizer(Discretizer):
            # [HP, HP, HP, HP], [LP, LP, LP, LP], # unfair combo
            # [LEFT, LK], [RIGHT, LK], # podnozhka :)
            # [DOWN, HP] # uppercut
-            [RIGHT, RUN]
+            [RIGHT, RUN],
+            [RIGHT, RUN, RUN],
+            [RIGHT, RUN, RUN, RUN],
+            [RIGHT, RUN, RUN, RUN, RUN],
+            [RIGHT, RUN, RUN, RUN, RUN, RUN]
            ])
 
 gamename = "MortalKombat3-Genesis"
